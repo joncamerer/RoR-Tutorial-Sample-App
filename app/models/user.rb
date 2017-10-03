ddff1374b143e27376415d58eb02df04b7a6ff72
Jon Camerer <jon.camerer@gmail.com>
2017-10-03 13:33:36 -0500

    Use cookies to handle a persistent session

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,11 +19,23 @@ class User < ApplicationRecord
 
   #Returns a random token.
   def User.new_token
-    SercureRandom.urlsafe_base64
+    SecureRandom.urlsafe_base64
   end
 
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    Bcrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
